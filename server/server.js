89e461a4b19c301b915278f7976e82efe3400a1e
Jobin Tom <jobint911@gmail.com>
2024-04-16 00:08:52 +0530

    Backend working

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-console.log("server")
+const express = require("express");
+const axios = require('axios');
+const cors = require("cors");
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+async function connectDB() {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Connected to MongoDB");
+}
+
+connectDB();
+
+const port = process.env.PORT || 3000;
+app.listen(port,() => {
+    console.log("Server is running on port 3000");
+});
+
