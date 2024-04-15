a282f516d3b3a2761c7a7e2bd6e93720c2ac41be
Jobin Tom <jobint911@gmail.com>
2024-04-16 00:24:31 +0530

    MongoDB connected

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const axios = require('axios');
 const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 
 const app = express();
