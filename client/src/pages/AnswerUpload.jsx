ab180df21eca059b93904b28bd9cebd0b27118e6
jiya <jiahofficial64@gmail.com>
2024-04-21 01:34:25 +0530

    made changes to upload option in answerupload page

--- client/src/pages/AnswerUpload.jsx
+++ client/src/pages/AnswerUpload.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 
 const AnswerUpload = () => {
+  const [files, setFiles] = useState([]);
+
+  const handleFileChange = (event) => {
+    // Access the selected files from the event
+    const selectedFiles = Array.from(event.target.files);
+    // Update the state with the selected files
+    setFiles((prevFiles) => [...prevFiles, ...selectedFiles]);
+  };
+
+  const handleUpload = () => {
+    // Perform upload logic with the selected files
+    if (files.length > 0) {
+      console.log('Uploading files:', files);
+      // Add your upload logic here
+    } else {
+      console.log('No files selected');
+    }
+  };
+
   const labelStyle = {
     position: 'absolute',
     top: '75px',
@@ -56,11 +75,32 @@ const AnswerUpload = () => {
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div style={labelStyle}>Flat 1</div>
       <div style={uploadLabelStyle}>Upload answer paper</div>
-      <button style={buttonStyle}>Upload(s)</button>
-      <button style={evaluateButtonStyle}>Evaluate</button>
+      <input
+        type="file"
+        onChange={handleFileChange}
+        style={{ display: 'none' }} // Hide the input element visually
+        accept=".png,.jpeg,.jpg"
+        multiple // Allow multiple file selection
+      />
+      <button style={buttonStyle} onClick={() => document.querySelector('input[type="file"]').click()}>
+        Upload(s)
+      </button>
+      <button style={evaluateButtonStyle} onClick={handleUpload}>
+        Evaluate
+      </button>
+      {/* Render the names of uploaded files */}
+      <div style={{ position: 'absolute', top: 'calc(30vh + 50px + 1.5cm)', left: '20px', zIndex: '9999' }}>
+        {files.length > 0 && (
+          <ul>
+            {files.map((file, index) => (
+              <li key={index}>{file.name} uploaded</li>
+            ))}
+          </ul>
+        )}
+      </div>
       {/* Your other content goes here */}
     </div>
   );
